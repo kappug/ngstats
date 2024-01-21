849ce8692ce7a1dec176daba7d522a088c4f22e4
Kappug <phinazim@gmail.com>
2024-01-21 17:41:22 -0500

    fix: Leaderboard command was using help arguments for some reason

--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -1,33 +1,34 @@
 import { Command } from "../handlers/commands";
-import { Gamemode } from "../utils/statEntries";
+import { Gamemode, gamemodes } from "../utils/statEntries";
 
 const symbols = ["❶", "❷", "❸", "❹", "❺", "❻", "❼", "❽", "❾", "❿"];
 
 export default <Command<[Gamemode | null]>>{
-	aliases: ["leaderboard", "lb"],
-	description: "Displays the top 10 players for a certain leaderboard.",
-	arguments: [
-		{
-			name: "command",
-			type: "string",
-			required: false,
-		},
-	],
-	execute: async (instance, _author, args) => {
-		const leaderboard = await instance.apiClient.leaderboard.list("game", {
-			column: (args[0] === "all" ? "wins" : `${args[0]}_wins`) as "wins",
-		});
+  aliases: ["leaderboard", "lb"],
+  description: "Displays the top 10 players for a certain leaderboard.",
+  arguments: [
+    {
+      name: "gamemode",
+      type: "string",
+      stringOptions: gamemodes as readonly string[],
+      required: true,
+    },
+  ],
+  execute: async (instance, _author, args) => {
+    const leaderboard = await instance.apiClient.leaderboard.list("game", {
+      column: (args[0] === "all" ? "wins" : `${args[0]}_wins`) as "wins",
+    });
 
-		let message = "⁎ Leaderboard (top 10 wins):";
+    let message = "⁎ Leaderboard (top 10 wins):";
 
-		for (let i = 0; i < 10; i++) {
-			const entry = leaderboard[i]!;
+    for (let i = 0; i < 10; i++) {
+      const entry = leaderboard[i]!;
 
-			message += ` ${symbols[i]} ${
-				entry.player
-			}: ${entry.value.toLocaleString()}`;
-		}
+      message += ` ${symbols[i]} ${
+        entry.player
+      }: ${entry.value.toLocaleString()}`;
+    }
 
-		return message;
-	},
+    return message;
+  },
 };
