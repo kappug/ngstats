ffd2d9071f51578063e03428dab76e1891015c3c
Kappug <69259502+Kappug@users.noreply.github.com>
2024-01-26 20:16:39 -0500

    fix: Reconnecting and same-lobby commands

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,54 @@ import { registerEvents } from "./handlers/events";
 import { registerCommands } from "./handlers/commands";
 
 async function main() {
-  const client = createClient({
-    version: process.env.VERSION as Version,
-    username: process.env.BOT_GAMERTAG,
-    host: process.env.NG_SERVER_ADDRESS ?? "play.nethergames.org",
-    port: parseInt(process.env.NG_SERVER_PORT ?? "19132"),
-  });
+	let hasConnected = false;
+	const client = createClient({
+		version: process.env.VERSION as Version,
+		username: process.env.BOT_GAMERTAG,
+		host: process.env.NG_SERVER_ADDRESS ?? "play.nethergames.org",
+		port: parseInt(process.env.NG_SERVER_PORT ?? "19132"),
+	});
 
-  const apiClient = new NetherGamesClient(process.env.NG_API_KEY, {
-    userAgent: `ngstats/${process.env.npm_package_version}`,
-  });
+	const apiClient = new NetherGamesClient(process.env.NG_API_KEY, {
+		userAgent: `ngstats/${process.env.npm_package_version}`,
+	});
 
-  const instance: Instance = {
-    client,
-    apiClient,
-    rankedChatWebhookClient: new WebhookClient({
-      url: process.env.RANKED_CHAT_WEBHOOK ?? "",
-    }),
-    logsWebhookClient: new WebhookClient({
-      url: process.env.LOGS_WEBHOOK ?? "",
-    }),
-    commands: [],
-    session: {},
-  };
+	const instance: Instance = {
+		client,
+		apiClient,
+		rankedChatWebhookClient: new WebhookClient({
+			url: process.env.RANKED_CHAT_WEBHOOK ?? "",
+		}),
+		logsWebhookClient: new WebhookClient({
+			url: process.env.LOGS_WEBHOOK ?? "",
+		}),
+		commands: [],
+		session: {},
+	};
 
-  console.log("Connecting...");
+	console.log("Connecting...");
 
-  client.on("join", () => {
-    console.log("Connected!");
+	client.on("join", () => {
+		console.log("Connected!");
+		hasConnected = true;
 
-    registerEvents(instance);
-    registerCommands(instance);
-  });
+		registerEvents(instance);
+		registerCommands(instance);
+	});
 
-  client.on("disconnect", ({ message }: { message: string }) => {
-    console.log(`Disconnected: ${message}\nReconnecting...`);
+	client.on("disconnect", async ({ message }: { message: string }) => {
+		console.log(`Disconnected: ${message}\nReconnecting...`);
 
-    main();
-  });
+		const hasConnected = await main();
+
+		setTimeout(() => {
+			if (!hasConnected) {
+				client.disconnect();
+			}
+		}, 20000);
+	});
+
+	return hasConnected;
 }
 
 config();
